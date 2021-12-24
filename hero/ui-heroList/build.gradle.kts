c7059c1b5bdb0fc66101b96f9ce3f8d0910f147a
Parkduksung <duksung1234@naver.com>
2021-12-24 13:14:28 +0900

    [hero-ui][feature] ui 관련 gradle 수정.

---
+++ hero/ui-heroList/build.gradle.kts
@@ -0,0 +1,14 @@
+apply {
+    from("$rootDir/android-library-build.gradle")
+}
+
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+}
+
+dependencies {
+    "implementation"(project(Modules.heroDomain))
+    "implementation"(project(Modules.heroInteractors))
+
+    "implementation"(Coil.coil)
+}
