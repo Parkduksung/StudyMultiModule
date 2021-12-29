e18e9c5b4af80800fbf8e850f81950531f681123
Parkduksung <duksung1234@naver.com>
2021-12-29 22:44:43 +0900

    [ui-heroList][feature] HeroListState 추가.

---
+++ hero/ui-heroList/src/main/java/com/example/ui_herolist/ui/HeroListState.kt
@@ -0,0 +1,28 @@
+package com.example.ui_herolist.ui
+
+
+import com.example.core.domain.*
+import com.example.hero_domain.Hero
+import com.example.hero_domain.HeroAttribute
+import com.example.hero_domain.HeroFilter
+
+/**
+ * @param progressBarState: State of the progress bar in UI.
+ * @param heros: unfiltered list of heros.
+ * @param filteredHeros: filtered list of heros.
+ * @param filterDialogState: Show/hide the dialog with filter options
+ * @param heroFilter: Filter heros by 'name' (asc / desc) or 'pro wins' (asc / desc)
+ * @param primaryAttrFilter: Filter heros by their primary attribute
+ * @param heroName: Filter heros by their name containing this string.
+ * @param errorQueue: Errors (will be shown as dialog or none and logged)
+ */
+data class HeroListState(
+    val progressBarState: ProgressBarState = ProgressBarState.Idle,
+    val heros: List<Hero> = listOf(),
+    val filteredHeros: List<Hero> = listOf(),
+    val filterDialogState: UIComponentState = UIComponentState.Hide, // show/hide the filter dialog
+    val heroFilter: HeroFilter = HeroFilter.Hero(FilterOrder.Descending),
+    val primaryAttrFilter: HeroAttribute = HeroAttribute.Unknown,
+    val heroName: String = "",
+    val errorQueue: Queue<UIComponent> = Queue(mutableListOf())
+)
